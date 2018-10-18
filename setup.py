c0f85ba6bd2aec502d5fb39a045f9e8e78d9ca47
Marius Gedminas <marius@gedmin.as>
2018-10-19 00:58:55 +0300

    Switch to new Framework :: Zope :: 3 classifier
    See https://github.com/zopefoundation/z3c.authviewlet/pull/6#discussion_r226414566
    for the background behind this.

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Topic :: Internet :: WWW/HTTP',
-        'Framework :: Zope3',
+        'Framework :: Zope :: 3',
     ],
     url='http://pypi.python.org/pypi/keas.profile',
     packages=find_packages('src'),
